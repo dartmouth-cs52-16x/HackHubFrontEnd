452ee548c773e73aa2fdb6ff36a70afb534552cb
jeanzhou31 <jeanzhou31@gmail.com>
2016-08-11 02:27:36 -0400

    added announcements

--- src/components/announcements.js
+++ src/components/announcements.js
@@ -1,1 +1,64 @@
-// like the blog, organizers are able to make announcements
+// navbar component -- home and new post buttons
+
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import NewAnnouncement from './new_announcement';
+
+class Announcement extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      announcements: [],
+      test: null,
+    };
+
+    this.createAnnouncement = this.createAnnouncement.bind(this);
+    this.renderAnnouncements = this.renderAnnouncements.bind(this);
+  }
+
+  // create a new note
+  createAnnouncement(input) {
+    this.state.announcements.push(input);
+    const newTest = input;
+    this.setState({
+      test: newTest,
+    });
+  }
+
+  renderAnnouncements() {
+    // let renderList = '';
+    // let ann = '';
+    /* for (let i = 0; i < this.state.announcements.length; i++) {
+      ann = this.state.announcements[i];
+      renderList = renderList + ' ' + ann;
+    } */
+    const renderList = this.state.announcements.map(text => {
+      return (
+        <div key={text} className="announcementsingle">
+          <div className="announcetext">
+            {text}
+          </div>
+          <div className="announcedate">
+            DATE
+          </div>
+        </div>
+      );
+    });
+    return renderList;
+  }
+
+
+  render() {
+    return (
+      <div className="announcementbox">
+        <NewAnnouncement createAnnouncement={this.createAnnouncement} />
+        <div className="allannouncements">
+          {this.renderAnnouncements()}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Announcement;
--- src/components/companies.js
+++ src/components/companies.js
@@ -1,1 +1,10 @@
-// companies listed here and link to their opportunities that people can click on
+// use an api for scheduling
+import React from 'react';
+
+export default function Companies(props) {
+  return (
+    <div id="companies">
+      <p>There will be companies here soon.</p>
+    </div>
+  );
+}
