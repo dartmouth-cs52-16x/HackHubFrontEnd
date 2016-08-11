361c25dd0d9d5c2f32ed9c51c04d36f161c16033
Robert Sayegh <sayeghro@gmail.com>
2016-08-11 18:11:21 -0400

    reverted emmas fixes

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ const ROOT_URL = 'http://localhost:9090/api';
 // keys for actiontypes
 export const ActionTypes = {
   FETCH_ANNS: 'FETCH_ANNS',
-  CREATE_ANN: 'CREATE_ANN',
+  CREATE_ANNS: 'CREATE_ANNS',
   FETCH_COMP: 'FETCH_COMP',
   CREATE_COMP: 'CREATE_COMP',
 };
@@ -28,11 +28,13 @@ export function createAnnouncement(ann) {
   console.log('created');
   return (dispatch) => {
     const fields = { text: ann.text, date: ann.date };
-    axios.post(`${ROOT_URL}/announcement`, fields).then(response => {
-      browserHistory.push('/');
-      browserHistory.push('/announcement');
+    axios.post(`${ROOT_URL}/announcement`, fields).then(() => {
+      axios.get(`${ROOT_URL}/announcement`).then(response => {
+        dispatch({ type: ActionTypes.CREATE_ANNS, payload: { all: response.data } });
+      }).catch(error => {
+        console.log('Error getting posts');
+      });
     }).catch(error => {
-      console.log(error);
       console.log('Error creating post');
     });
   };
