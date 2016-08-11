555f730dc6b1b61a3c1f8dbe9e6f10e5ca35dad8
emmaoberstein <emmaoberstein@gmail.com>
2016-08-11 19:08:18 -0400

    front end deletion

--- src/reducers/company-reducer.js
+++ src/reducers/company-reducer.js
@@ -1,10 +1,6 @@
 import { ActionTypes } from '../actions';
 
-<<<<<<< HEAD
-const CompanyReducer = (state = { all: [], currentCompany: null }, action) => {
-=======
 const CompanyReducer = (state = { all: [] }, action) => {
->>>>>>> f08c39e39f26b7a179e1753565c17a37c032ba61
   switch (action.type) {
     case ActionTypes.FETCH_COMP:
       return { ...state, all: action.payload.all };
