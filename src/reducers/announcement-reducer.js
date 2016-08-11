cfa2126a4d09816dee82c68dcd106a751525b826
Robert Sayegh <sayeghro@gmail.com>
2016-08-11 17:45:57 -0400

    fixes to announcements and companies

--- src/reducers/announcement-reducer.js
+++ src/reducers/announcement-reducer.js
@@ -3,11 +3,11 @@ import { ActionTypes } from '../actions';
 const AnnouncementReducer = (state = { all: [], currentPost: null }, action) => {
   switch (action.type) {
     case ActionTypes.FETCH_ANNS:
-      return { all: action.payload, currentPost: state.currentPost };
+      return { ...state, all: action.payload, currentPost: state.currentPost };
     case ActionTypes.FETCH_POST:
-      return { all: [], currentPost: action.payload };
+      return { ...state, all: [], currentPost: action.payload };
     case ActionTypes.CREATE_ANN:
-      return { all: action.payload, currentPost: state.currentPost };
+      return { ...state, all: action.payload, currentPost: state.currentPost };
     default:
       return state;
   }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import AnnouncementReducer from './announcement-reducer';
+import CompanyReducer from './company-reducer';
 
 const rootReducer = combineReducers({
   announcements: AnnouncementReducer,
+  companies: CompanyReducer,
 });
 
 export default rootReducer;
