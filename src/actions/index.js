28a309fa6566f71a067fe1176e910e3e0ff77c09
emmaoberstein <emmaoberstein@gmail.com>
2016-08-24 15:24:14 -0400

    fixing stuff

--- src/actions/index.js
+++ src/actions/index.js
@@ -397,7 +397,7 @@ export function updateSchedule(input) {
     };
     axios.put(`${ROOT_URL}/schedule/${fields.id}`, fields).then(response => {
       console.log(response.data);
-      dispatch({ type: ActionTypes.UPDATE_SCHED, payload: { all: response.data } });
+      // dispatch({ type: ActionTypes.UPDATE_SCHED, payload: { all: response.data } });
       browserHistory.push('/schedule');
     }).catch(error => {
       console.log(error);
