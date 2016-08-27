e17097df6177d69bc5d94a68f800733c3a4492b4
emmaoberstein <emmaoberstein@gmail.com>
2016-08-27 16:52:51 -0400

    reset events

--- src/components/schedule.js
+++ src/components/schedule.js
@@ -38,32 +38,7 @@ class Schedule extends Component {
           start: 10,
           end: 20,
         },
-        events: [
-          {
-            time_range: '10-11am',
-            name: 'Sample Event 1',
-            location: 'A Place',
-            color: '#8BB9B6',
-            start: 10.0,
-            end: 11.0,
-          },
-          {
-            time_range: '1pm-3pm',
-            name: 'Sample Event 2',
-            location: 'Another Place',
-            color: '#E0CC5C',
-            start: 13.0,
-            end: 15.0,
-          },
-          {
-            time_range: '7pm-8pm',
-            name: 'Sample Event 2',
-            location: 'Another Place',
-            color: '#8BB9B6',
-            start: 19.0,
-            end: 20.0,
-          },
-        ],
+        events: [],
       },
       day2: {
         day_of_week: 'Day of Week',
@@ -73,32 +48,7 @@ class Schedule extends Component {
           start: 10,
           end: 20,
         },
-        events: [
-          {
-            time_range: '9-10am',
-            name: 'Sample Event 1',
-            location: 'A Place',
-            color: '#8BB9B6',
-            start: 9.0,
-            end: 10.0,
-          },
-          {
-            time_range: '3pm-5pm',
-            name: 'Sample Event 2',
-            location: 'Another Place',
-            color: '#E0CC5C',
-            start: 15.0,
-            end: 17.0,
-          },
-          {
-            time_range: '11pm-11:30pm',
-            name: 'Sample Event 2',
-            location: 'Another Place',
-            color: '#8BB9B6',
-            start: 23.0,
-            end: 23.5,
-          },
-        ],
+        events: [],
       },
     });
   }
